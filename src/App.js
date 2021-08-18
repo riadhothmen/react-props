572993b33ab74fa3e82d931fee957ccddca1e233
riadhothmen <r.benothmann@yahoo.com>
2021-08-18 09:54:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import React from 'react';
-import './profile/profile.css';
+import React, { Component } from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Profile from './profile/profile';
 
 
-function App() {
-  function handleName() {
-    return alert("Riadh BEN OTHMANE");
-  }
-  return (
-    <div className="App">
-      <header  className="App-header">
-        <Profile Email="r.benothmann@yahoo.com"  Phone="21203049" FullName="BEN OTHMANE Riadh"  Bio="I'm 39 Years old , Passionated with web development , trying to build my carrer in this field " Profession="job seeker"  Adress="Sousse">
-          <img  style={{ borderRadius: '5px', width : '100px', height: '100px'}} alt="User-Profile-Image" src='riadh.jpg' alt='abc'></img>
-           { handleName() }
-          </Profile>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  constructor(props) {
+    super(props);
+  
+    this.state = { 
+      number: 1,
+      person : {
+      FullName: "Riadh BEN OTHMANE",
+      Profession: "job seeker",
+      Bio : 'Passionated by Web development',
+      Show: true
+    }}
+ };
+
+// to toggle the boolean state 
+
+ toggleButton =()=> {
+  this.setState ({
+    number: 1,
+    person : { Show : ! this.state.person.Show,
+    FullName: "Riadh BEN OTHMANE",
+      Profession: "job seeker",
+      Bio : 'Passionated by Web development',
+  } })
+ };
+
+
+//  To set the counter
+
+
+ componentDidMount() {
+  this.interval = setInterval(() => {
+    console.log(this.state.number, this.props.counter)
+    if (this.props.counter === 'double') {
+      this.setState({number: this.state.number + this.state.number});        
+    } else {
+      this.setState({number: this.state.number + 1});
+    }
+  }, 1000);
 }
 
+componentWillUnmount() {
+  clearInterval(this.interval);
+}
+
+
+// to set the HTML
+ buttonToggled =() => {
+   return ( <div className='btnToggled'>
+     {this.state.person.Show ? <p></p> : <div>
+      <p>{this.state.number} seconds since the componentDidMount</p>
+      <div className="card" style={{width: '18rem'}}>
+  <img src="riadh.jpg" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h3 className="card-title">{this.state.person.FullName}</h3>
+    <h5 className="card-title">{this.state.person.Profession}</h5>
+    <p className="card-text">{this.state.person.Bio}</p>   
+  </div>
+</div>
+     </div>}
+   </div>)};
+
+
+ 
+  render() {
+
+    return(
+     <div className="AppIn">
+       <button  onClick = {(e) =>  { this.toggleButton() ;
+      }} type="button" class="btn btn-primary">View The profile </button>
+       {this.buttonToggled()}
+      </div>  
+);
+  }
+ 
+}
 export default App;
